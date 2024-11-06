f75aa1d02bfc821a2b993799e021ade28e47844e
Bobrov27 <aleksandr2707bobrov@gmail.com>
2024-11-06 16:46:32 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from math import pi
-
 class Figure:
     def __init__(self): #p = (a+b+c)/2 #sqrt (p*(p-a)(p-b)(p-c))
           pass
@@ -7,35 +6,23 @@ class Figure:
         pass
 
 
+class Figure:
+    pass
 
 
-class triangle(Figure):
+
+class Triangle(Figure):
     def __init__(self, a, b, c):
         self.a = a
         self.b = b
         self.c = c
 
     def calculate_area(self):
-        p =
+        p = (self.a + self.b + self.c) /2
+        return (p*(p-self.a)*(p-self.b)*(p-self.c))**0.5
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+My_Triangle = Triangle (3,4,5)
+print(My_Triangle.calculate_area())
 
 
 class Rectangle(Figure):
@@ -49,6 +36,7 @@ class Rectangle(Figure):
 My_Rectangle = Rectangle(5,4)
 print(My_Rectangle.calculate_area())
 
+
 class Circle(Figure):
     def __init__(self, radius):
         self.radius = radius
@@ -57,19 +45,3 @@ class Circle(Figure):
         return pi*self.radius**2
 My_Circle = Circle(6)
 print(My_Circle.calculate_area())
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
